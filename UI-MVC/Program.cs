243f61f492e3107b2bf19c07c35aba6ee1772e96
loic-vermeire <loic.vermeire@student.kdg.be>
2019-04-05 18:24:35 +0200

    url program.cs

--- UI-MVC/Program.cs
+++ UI-MVC/Program.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore;
+﻿using System.IO;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 
 namespace SC.UI.Web.MVC
@@ -12,6 +13,7 @@ namespace SC.UI.Web.MVC
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .UseStartup<Startup>()
+                .UseUrls("http://*:80");
     }
 }
